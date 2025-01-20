73365a65f0a740514157aba2955d3e614d501174
Ben Do <bdo.appworkshop@gmail.com>
2025-01-20 14:55:02 -0600

    Wrapped NavigationMenu around the app.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/globals.css";
+import NavigationMenu from "@/components/navigation/navigation-menu";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+      <NavigationMenu>
+        <Component {...pageProps} />
+      </NavigationMenu>
+  );
 }
