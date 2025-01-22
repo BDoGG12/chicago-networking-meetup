b891c697f9a5ba14a6becc18ac64fea2c311c223
Ben Do <bdo.appworkshop@gmail.com>
2025-01-22 09:36:17 -0600

    🔧 Added firebase configuration.

---
+++ services/firebase.config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnMYW1K8S8JjUwG0e_02FeSdnmn19RujY",
+  authDomain: "chicago-networking-meetup.firebaseapp.com",
+  projectId: "chicago-networking-meetup",
+  storageBucket: "chicago-networking-meetup.firebasestorage.app",
+  messagingSenderId: "91724314366",
+  appId: "1:91724314366:web:157652ac5c25ed485017af"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+export {auth};
