e65bded51050e20b6280adbd202da1c6a71645d0
Ben Do <bdo.appworkshop@gmail.com>
2025-01-27 10:01:53 -0600

    Replaced refs with state variables, implemented handlers for userName and password, imported validation hooks.

--- components/sign-in/sign-in.js
+++ components/sign-in/sign-in.js
@@ -1,18 +1,46 @@
 import { Button, Form } from "react-bootstrap";
 import { useState } from "react";
+import { auth } from "@/services/firebase.config";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "next/router";
+import { isEmail, validatePassword } from "@/hooks/validationHooks";
 
 const SignIn = () => {
-    const [validated, setValidated] = useState(false);
-    
-    const handleSubmit = (e) => {
-        const form = e.currentTarget;
-        if (form.checkValidity() === false) {
-            e.preventDefault();
-            e.stopPropagation();
-        }
+  const [validated, setValidated] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
 
-        setValidated(true);
-    }
+  const handleUserName = (e) => {
+    e.preventDefault();
+
+    const userNameVal = e.target.value;
+    setUserName(userNameVal)
+  };
+
+  const handlePassword = (e) => {
+    e.preventDefault();
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    const form = e.currentTarget;
+
+    signInWithEmailAndPassword(auth, userName, password)
+      .then((userObject) => {
+        const { user } = userObject;
+        const isEmailValid = isEmail(userName);
+        const isPasswordValid = validatePassword(password);
+        if (user && isEmailValid && isPasswordValid) {
+          router.push("/");
+        }
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+    e.preventDefault();
+    setValidated(true);
+  };
   return (
     <div>
       <h2>Sign In</h2>
@@ -20,12 +48,24 @@ const SignIn = () => {
       <Form noValidate validated={validated} onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control required type="email" placeholder="Enter email" />
+          <Form.Control
+            required
+            value={userName}
+            onChange={handleUserName}
+            type="email"
+            placeholder="Enter email"
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control required type="password" placeholder="Password" />
+          <Form.Control
+            required
+            value={password}
+            onChange={handlePassword}
+            type="password"
+            placeholder="Password"
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
--- components/sign-up/sign-up.js
+++ components/sign-up/sign-up.js
@@ -1,18 +1,34 @@
 import { Button, Form } from "react-bootstrap";
 import { useState } from "react";
+import { auth } from "@/services/firebase.config";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "next/router";
+import { isEmail, validatePassword } from "@/hooks/validationHooks";
 
 const SignUp = () => {
-    const [validated, setValidated] = useState(false);
-    
-    const handleSubmit = (e) => {
-        const form = e.currentTarget;
-        if (form.checkValidity() === false) {
-            e.preventDefault();
-            e.stopPropagation();
-        }
+  const [validated, setValidated] = useState(false);
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
 
-        setValidated(true);
-    }
+  const handleSubmit = async (e) => {
+    const form = e.currentTarget;
+    
+    createUserWithEmailAndPassword(auth, userName, password)
+      .then((userObject) => {
+        const { user } = userObject;
+        const isEmailValid = isEmail(userName);
+        const isPasswordValid = validatePassword(password);
+        if (user && isEmailValid && isPasswordValid) {
+          router.push("/");
+        }
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+    e.preventDefault();
+    setValidated(true);
+  };
   return (
     <div>
       <h2>Sign Up</h2>
@@ -20,12 +36,24 @@ const SignUp = () => {
       <Form noValidate validated={validated} onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control required type="email" placeholder="Enter email" />
+          <Form.Control
+            required
+            value={userName}
+            type="email"
+            placeholder="Enter email"
+            onChange={setUserName}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control required type="password" placeholder="Password" />
+          <Form.Control
+            required
+            value={password}
+            type="password"
+            placeholder="Password"
+            onChange={setPassword}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Confirm Password</Form.Label>
