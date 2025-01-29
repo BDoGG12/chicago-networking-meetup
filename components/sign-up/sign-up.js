1be8bc8a1edd6b4c8cbc4b963e96662d6fa8b9b6
Ben Do <bdo.appworkshop@gmail.com>
2025-01-29 11:09:16 -0600

    Added state variables for confirmed password input, implemented handlers for userName, password, and confirmed password.

--- components/sign-up/sign-up.js
+++ components/sign-up/sign-up.js
@@ -9,24 +9,47 @@ const SignUp = () => {
   const [validated, setValidated] = useState(false);
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmedPassword, setConfirmedPassword] = useState('');
+  const [isPasswordConfirmed, setIsPasswordConfirmed] = useState(false);
   const router = useRouter();
+
+  const handleUserName = (e) => {
+    setUserName(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmedPassword = (e) => {
+    setConfirmedPassword(e.target.value);
+  };
+
+  const confirmPassword = (enteredPassword) => {
+    if (e.target.value === password) {
+      setIsPasswordConfirmed(true);
+    } else {
+      setIsPasswordConfirmed(false);
+    }
+  };
 
   const handleSubmit = async (e) => {
     const form = e.currentTarget;
-    
+    console.log('form', form)
+    e.preventDefault();
     createUserWithEmailAndPassword(auth, userName, password)
       .then((userObject) => {
         const { user } = userObject;
+        confirmPassword(confirmedPassword);
         const isEmailValid = isEmail(userName);
         const isPasswordValid = validatePassword(password);
-        if (user && isEmailValid && isPasswordValid) {
+        if (user && isEmailValid && isPasswordValid && isPasswordConfirmed) {
           router.push("/");
         }
       })
       .catch((error) => {
         console.log(error.message);
       });
-    e.preventDefault();
     setValidated(true);
   };
   return (
@@ -41,7 +64,7 @@ const SignUp = () => {
             value={userName}
             type="email"
             placeholder="Enter email"
-            onChange={setUserName}
+            onChange={handleUserName}
           />
         </Form.Group>
 
@@ -52,12 +75,18 @@ const SignUp = () => {
             value={password}
             type="password"
             placeholder="Password"
-            onChange={setPassword}
+            onChange={handlePassword}
           />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Group className="mb-3" controlId="formConfirmPassword">
           <Form.Label>Confirm Password</Form.Label>
-          <Form.Control required type="password" placeholder="Password" />
+          <Form.Control 
+            required 
+            value={confirmedPassword}
+            type="password" 
+            placeholder="Password"
+            onChange={handleConfirmedPassword}
+            />
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
