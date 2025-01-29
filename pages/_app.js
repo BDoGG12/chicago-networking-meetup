909426a7a195f7524cb56a443c343086ab7e9f67
Ben Do <bdo.appworkshop@gmail.com>
2025-01-29 11:10:17 -0600

    Wrapped UserProvider around child components.

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
 import NavigationMenu from "@/components/navigation/navigation-menu";
+import { UserProvider } from "@/context/userContext";
 
 export default function App({ Component, pageProps }) {
   return (
-      <NavigationMenu>
+    <NavigationMenu>
+      <UserProvider>
         <Component {...pageProps} />
-      </NavigationMenu>
+      </UserProvider>
+    </NavigationMenu>
   );
 }
