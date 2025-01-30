8ebc9b245108053a4c0b28c55284ce35b6eab4d9
Ben Do <bdo.appworkshop@gmail.com>
2025-01-30 12:01:38 -0600

    Implemented signout handler, conditionally rendered navigation menus for signed in users.

--- components/navigation/navigation-menu.js
+++ components/navigation/navigation-menu.js
@@ -1,7 +1,33 @@
-import { Nav, Container, Navbar, Form, Button } from "react-bootstrap";
+import {
+  Nav,
+  Container,
+  Navbar,
+  Form,
+  Button,
+  NavDropdown,
+} from "react-bootstrap";
 import classes from "./navigation-menu.module.css";
+import { UserContext } from "@/context/userContext";
+import { useContext } from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "@/services/firebase.config";
+import { useRouter } from "next/router";
 
 const NavigationMenu = ({ children }) => {
+  const router = useRouter();
+  const { isLoggedIn, setIsLoggedIn } = useContext(UserContext);
+
+  const handleSignOut = async () => {
+    signOut(auth)
+      .then(() => {
+        setIsLoggedIn(false);
+        router.push("/");
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
   return (
     <>
       <Navbar bg="dark" data-bs-theme="dark">
@@ -16,8 +42,19 @@ const NavigationMenu = ({ children }) => {
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/events">Events</Nav.Link>
-            <Nav.Link href="/sign-up">Sign Up</Nav.Link>
-            <Nav.Link href="/sign-in">Sign In</Nav.Link>
+            {!isLoggedIn && (
+              <>
+                <Nav.Link href="/sign-up">Sign Up</Nav.Link>
+                <Nav.Link href="/sign-in">Sign In</Nav.Link>
+              </>
+            )}
+            {isLoggedIn && (
+              <NavDropdown title="Your Account" id="your-account">
+                <NavDropdown.Item onClick={handleSignOut}>
+                  Sign Out
+                </NavDropdown.Item>
+              </NavDropdown>
+            )}
           </Nav>
         </Container>
       </Navbar>
