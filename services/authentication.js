ed2fb1d8f78ad89d30a3281bf9c668a351cab29c
Ben Do <bdo.appworkshop@gmail.com>
2025-01-30 13:42:02 -0600

    Implemented authentication hooks.

--- services/authentication.js
+++ services/authentication.js
@@ -1,20 +1,21 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "@/services/firebase.config";
 
-const signUpUser = (email, password) => {
+const signUpUser = async (email, password) => {
 
     createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             // signed up
             const user = userCredential.user
+            console.log(user);
         })
         .catch((error) => {
-            const errorCode = error.code;
             const errorMessage = error.message;
+            console.log(errorMessage);
         });
 };
 
-const signInUser = (email, password) => {
+const signInUser = async (email, password) => {
     signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
         // signed in
