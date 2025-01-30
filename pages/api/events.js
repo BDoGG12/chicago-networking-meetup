56d5e6391ab4c6e267f7ab7a5b4e36bb1641a261
Ben Do <bdo.appworkshop@gmail.com>
2025-01-30 13:26:40 -0600

    Implemented api handler for fetching networking events in Chicago.

---
+++ pages/api/events.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const handler = async (req, res) => {
+
+    const {query} = req.query;
+
+    if (!query) {
+        return res.status(400).json({error: 'Query parameter is required'});
+    }
+
+    try {
+        const response = await axios.get('https://serpapi.com/search', {
+            params: {
+                engine: 'google_events',
+                q: 'Networking Events in Chicago',
+                api_key: process.env.private_api_key
+            },
+        });
+        res.status(200).json(response.data);
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+}
