b7e1bba19c3e1bbd6ad8d9126aa77b91d8f73d6b
Ben Do <bdo.appworkshop@gmail.com>
2025-01-30 13:25:39 -0600

    Added head title for Events page.

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,12 @@
+import Head from "next/head";
 
 const EventsPage = () => {
 
     return (
         <div>
+            <Head>
+                <title>Chicago Networking Meetup - Events</title>
+            </Head>
             <h1>Events Page</h1>
         </div>
     );
