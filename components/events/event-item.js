dbb04c92a06a1c0fbee47676665d2d5af995000e
Ben Do <bdo.appworkshop@gmail.com>
2025-02-03 15:25:50 -0600

    Added styling for cardItem.

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,12 +1,13 @@
 import { Card, Button } from "react-bootstrap";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import classes from "@/components/events/event-item.module.css";
 
 const EventItem = ({ title, imgSrc, description, link }) => {
   const router = useRouter();
 
   return (
-    <Card style={{ width: "18rem" }}>
+    <Card className={classes.cardItem}>
       <Card.Img variant="top" src={imgSrc} />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
