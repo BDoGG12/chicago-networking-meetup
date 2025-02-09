b0d1a2ea69adfaed4b7bdef8f797fbc10086ae74
Ben Do <bdo.appworkshop@gmail.com>
2025-02-09 09:15:32 -0600

    Implemented pop-up modals for events.

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,27 +1,38 @@
 import { Card, Button } from "react-bootstrap";
 import { useRouter } from "next/router";
-import Link from "next/link";
+import { useState } from "react";
 import classes from "@/components/events/event-item.module.css";
+import EventModal from "@/components/modals/event-modal";
 
 const EventItem = ({ title, imgSrc, description, link }) => {
-  const router = useRouter();
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
-    <Card className={classes.cardItem}>
-      <Card.Img variant="top" src={imgSrc} />
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>{description}</Card.Text>
-        <Link 
-        href={link}
-        target="_blank"
-        >
-          <Button variant="primary">
-            Get Tickets
-          </Button>
-        </Link>
-      </Card.Body>
-    </Card>
+    <div>
+      <Card className={classes.cardItem}>
+        <Card.Img variant="top" src={imgSrc} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{description}</Card.Text>
+          <Button variant="primary" onClick={handleShow}>View Event Detail</Button>
+        </Card.Body>
+      </Card>
+
+      {show && (
+        <>
+          <EventModal
+            show={show}
+            closeHandle={handleClose}
+            showHandle={handleShow}
+            title={title}
+            description={description}
+            link={link}
+          />
+        </>
+      )}
+    </div>
   );
 };
 
