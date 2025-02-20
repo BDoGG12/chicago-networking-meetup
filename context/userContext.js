53d1c5bfb32f27a2c4d69ed6b7198257ad3b6e1c
Ben Do <bdo.appworkshop@gmail.com>
2025-02-20 10:11:31 -0600

    Removed redundant export statement.

--- context/userContext.js
+++ context/userContext.js
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 
 const UserContext = createContext();
 
-export const UserProvider = ({children}) => {
+const UserProvider = ({children}) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     
     return (
